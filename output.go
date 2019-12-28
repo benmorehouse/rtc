416e053d69b62614c4059618aea818e8bfb3a810
Benjamin Morehouse <bmorehou@usc.edu>
2019-12-28 15:53:51 -0800

    udates to ver 1.0

---
+++ output.go
@@ -0,0 +1,59 @@
+package main
+
+import(
+	"github.com/gookit/color"
+	"fmt"
+)
+
+func (this* results) Output(input mode){
+	if this.NuetralThresholdReached{
+		color.Yellow.Println("*** ALERT *** \nDESIGNATED NUETRAL THRESHOLD HAS BEEN REACHED. \nSUGGESTION: CHECK RESCUETIME ACCOUNT FOR DISCREPENCY IN CATEGORIZATION")
+	}
+
+	if this.UnproductiveThresholdReached{
+		color.Yellow.Println("*** ALERT *** \nDESIGNATED UNPRODUCTIVE THRESHOLD HAS BEEN REACHED. \nSUGGESTION: CHECK RESCUETIME ACCOUNT FOR DISCREPENCY IN CATEGORIZATION")
+	}
+
+	if input == Day{
+		this.OutputHelper()
+	}else if input == Week{
+		return
+	}else{
+		return
+	}
+}
+
+func (this* results) OutputHelper(){
+	percentages := [5]float64{
+		(float64(this.VeryProductive)   / float64(this.TotalTimeSpent))*100,
+		(float64(this.Productive)       / float64(this.TotalTimeSpent))*100,
+		(float64(this.Nuetral)          / float64(this.TotalTimeSpent))*100,
+		(float64(this.Unproductive)     / float64(this.TotalTimeSpent))*100,
+		(float64(this.VeryUnproductive) / float64(this.TotalTimeSpent))*100,
+	}
+
+	for i:=0;i<5;i++{
+		switch i{
+		case 0:
+			color.Blue.Print("Very Productive    ")
+		case 1:
+			color.LightBlue.Print("Productive         ")
+		case 2:
+			color.Gray.Print("Nuetral            ")
+		case 3:
+			color.LightRed.Print("Unproductive       ")
+		case 4:
+			color.Red.Print("Very Unproductive  ")
+		}
+		fmt.Print("[")
+		for j:=0.0;j<50;j++{
+			if j > percentages[i]/2{
+				fmt.Print("-")
+			}else{
+				fmt.Print("#")
+			}
+		}
+
+		fmt.Println("]",percentages[i],"%")
+	}
+}
--- rtc.go
+++ rtc.go
@@ -1,10 +1,11 @@
 package main
 
 import(
+	"github.com/spf13/cobra"
 	"net/http"
 	"encoding/csv"
-	"github.com/spf13/cobra"
 	"strconv"
+//	"log"
 )
 
 type RTDay struct{
@@ -16,28 +17,30 @@ type RTDay struct{
 
 type results struct{
 	TotalTimeSpent uint
-	VeryUnproductive uint
-	Unproductive uint
-	Nuetral uint
-	Productive uint
 	VeryProductive uint
+	Productive uint
+	Nuetral uint
+	Unproductive uint
+	VeryUnproductive uint
+	NuetralThreshold uint
 	NuetralThresholdReached bool
+	UnproductiveThreshold uint
 	UnproductiveThresholdReached bool
 }
 
 type mode int
-const{
+const(
 	Day    mode = 0
-	Week   mode = 0
-	Month  mode = 0
-}
+	Week   mode = 1
+	Month  mode = 2
+)
 
-var todayCmd = &cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:"",
 	DisableFlagParsing:true, // all flags passed as arguments
 	Short:"Display rescuetime data",
 	Run: func(cmd *cobra.Command, args []string){ // args is gonna be what we pass through 
-		if len(args) == 0{ // default is to just do the day
+		if len(args) == 0 || args[0] == "d"{ // default is to just do the day
 			request := GenerateRequest() // generates our request for us
 			resp, err := http.Get(request)
 			if err != nil{
@@ -49,28 +52,58 @@ var todayCmd = &cobra.Command{
 				return
 			}
 
-			var parsedData []RTDay
-			for _, day := range data[1:]{
-				var scanned RTDay
-				scanned.Date = day[0]
-				scanned.TimeSpent, _ = strconv.Atoi(day[1])
-				scanned.NumberofPeople, _ = strconv.Atoi(day[2])
-				scanned.Productivity, _ = strconv.Atoi(day[3])
-				parsedData = append(parsedData,scanned)
+			bufferResults := results{
+				TotalTimeSpent: 0,
+				VeryUnproductive: 0,
+				Unproductive: 0,
+				Nuetral: 0,
+				Productive: 0,
+				VeryProductive: 0,
+				NuetralThreshold:10,
+				NuetralThresholdReached: false,
+				UnproductiveThreshold:10,
+				UnproductiveThresholdReached: false,
 			}
+
+			for _, day := range data[1:]{
+				timeSpentBuffer, _ := strconv.Atoi(day[1])
+				timeSpent := uint(timeSpentBuffer)
+				productivity, _ := strconv.Atoi(day[3])
+
+				bufferResults.TotalTimeSpent += uint(timeSpent)
+
+				switch productivity{
+				case -2:
+					bufferResults.VeryUnproductive += timeSpent
+				case -1:
+					bufferResults.Unproductive += timeSpent
+				case 0:
+					bufferResults.Nuetral += timeSpent
+				case 1:
+					bufferResults.Productive += timeSpent
+				case 2:
+					bufferResults.VeryProductive += timeSpent
+				}
+			}
+
+			if float64(bufferResults.Nuetral) / float64(bufferResults.TotalTimeSpent) < float64(bufferResults.NuetralThreshold){
+				bufferResults.NuetralThresholdReached = true
+			}
+
+			if float64(bufferResults.Unproductive + bufferResults.VeryUnproductive) / float64(bufferResults.TotalTimeSpent) < float64(bufferResults.UnproductiveThreshold){
+				bufferResults.NuetralThresholdReached = true
+			}
+
+			bufferResults.Output(Day)
+		}else if args[0] == "w" {
+			return
+			//bufferResults.Ouput(Week)
 		}else{
 			return
+			//bufferResults.Ouput(Week)
 		}
-	}
+	},
 }
 
 // can keep data within boltdb and can use a nice CLI interface maybe with cobra? Doesnt have to use cobra though.
-func (this* results) Output(input mode){
-	if input == Day{
 
-	}else if input == Week{
-		return
-	}else{
-		return
-	}
-}
