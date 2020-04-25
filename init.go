bcd42cd5ec7e879874edb286d6eb91ec007ba3b5
Benjamin Morehouse <bmorehou@usc.edu>
2020-04-24 23:47:48 -0700

    trying to get the configuration made here

--- init.go
+++ init.go
@@ -1,76 +1,90 @@
 package main
 
-import(
-	"github.com/boltdb/bolt"
-	"github.com/spf13/cobra"
-	"github.com/gookit/color"
+import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
-	"log"
+
+	"github.com/gookit/color"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	yaml "gopkg.in/yaml.v2"
 )
 
+type config struct {
+	LogFile string `yaml:"LogFile"`
+	Verbose bool   `yaml:"Verbose"`
+	Key     string `yaml:"Key"`
+}
+
+const configFileName string = ".rtc.yaml"
+
 var InitCmd = &cobra.Command{
-	Use:"init",
-	Short:"Initialize your Rescuetime CLI",
-	Run: func(cmd *cobra.Command, args []string){
+	Use:   "init",
+	Short: "Initialize your Rescuetime CLI",
+	Run: func(cmd *cobra.Command, args []string) {
+
 		color.LightBlue.Println("Welcome to your very own Rescue Time CLI")
-		for{
+		for {
 			fmt.Print("Have you created a rescuetime account yet? [Y/N]")
 			var input string
 			fmt.Scan(&input)
 			input = strings.ToUpper(input)
-			if input == "YES" || input == "Y"{
+			if input == "YES" || input == "Y" {
 				break
-			}else if input == "NO" || input == "N"{
+			} else if input == "NO" || input == "N" {
 				fmt.Print("Go to ")
 				color.Magenta.Print("https://www.rescuetime.com/")
 				fmt.Print(" and make an account. You must do so before continuing!")
 				return
-			}else{
+			} else {
 				fmt.Println("** Not a valid input **")
 				continue
 			}
 		}
+
 		fmt.Print("Next go to ")
 		color.LightBlue.Print("https://www.rescuetime.com/apidoc")
 		fmt.Println(" and create an API key. \nThis will act as your way of getting access to your account through this CLI.")
-		for{
+
+		var input string
+		for {
 			fmt.Print("Have you created a rescuetime API Key yet? [Y/N]")
-			var input string
 			fmt.Scan(&input)
 			input = strings.ToUpper(input)
-			if input == "YES" || input == "Y"{
+			if input == "YES" || input == "Y" {
 				break
-			}else if input == "NO" || input == "N"{
+			} else if input == "NO" || input == "N" {
 				fmt.Print("Go to ")
 				color.Magenta.Print("https://www.rescuetime.com/")
 				fmt.Print(" and make an account. You must do so before continuing!")
 				return
-			}else{
+			} else {
 				fmt.Println("** Not a valid input **")
 				continue
 			}
 		}
 
-		// now we have our APIKey
-		db, err := bolt.Open("RescueTimeDatabase.db", 0600, nil)
-		if err != nil{
-			return
-		}
-		defer db.Close()
-		_ = db.Update(func(tx *bolt.Tx) error{
-			fmt.Print("Paste your API Key here:")
-			var APIKey string
-			fmt.Scan(&APIKey)
-			bucket , err := tx.CreateBucketIfNotExists([]byte("apiAuth"))
-			err = bucket.Put([]byte("apiAuth"),[]byte(APIKey)) //creates the new bucket with nothing in it 
-			if bucket == nil{
-				log.Fatal(err," -> Possible cause is that bolt binary isnt installed correctly")
-			}
-			return nil
-		})
 	},
 }
 
+// getCurrentConfiguration will return the current configuration
+func getCurrentConfiguration() (*config, error) {
 
+	conf, err := os.Open(configFileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, err
+		}
+	}
+}
 
+// setLogger will set the logging for logrus, and will then take place through
+// the rest of the application.
+// NOTE: default file: rtc.log
+func setLogger() {
+
+	log.SetOutput(ioutil.Discard)
+	log.SetReportCaller(true)
+}
--- output.go
+++ output.go
@@ -1,39 +1,39 @@
 package main
 
-import(
-	"github.com/gookit/color"
+import (
 	"fmt"
+	"github.com/gookit/color"
 )
 
-func (this* results) Output(input mode){
-	if this.NuetralThresholdReached{
+func (this *results) Output(input mode) {
+	if this.NuetralThresholdReached {
 		color.Yellow.Println("*** ALERT *** \nDESIGNATED NUETRAL THRESHOLD HAS BEEN REACHED. \nSUGGESTION: CHECK RESCUETIME ACCOUNT FOR DISCREPENCY IN CATEGORIZATION")
 	}
 
-	if this.UnproductiveThresholdReached{
+	if this.UnproductiveThresholdReached {
 		color.Yellow.Println("*** ALERT *** \nDESIGNATED UNPRODUCTIVE THRESHOLD HAS BEEN REACHED. \nSUGGESTION: CHECK RESCUETIME ACCOUNT FOR DISCREPENCY IN CATEGORIZATION")
 	}
 
-	if input == Day{
+	if input == Day {
 		this.OutputHelper()
-	}else if input == Week{
+	} else if input == Week {
 		return
-	}else{
+	} else {
 		return
 	}
 }
 
-func (this* results) OutputHelper(){
+func (this *results) OutputHelper() {
 	percentages := [5]float64{
-		(float64(this.VeryProductive)   / float64(this.TotalTimeSpent))*100,
-		(float64(this.Productive)       / float64(this.TotalTimeSpent))*100,
-		(float64(this.Nuetral)          / float64(this.TotalTimeSpent))*100,
-		(float64(this.Unproductive)     / float64(this.TotalTimeSpent))*100,
-		(float64(this.VeryUnproductive) / float64(this.TotalTimeSpent))*100,
+		(float64(this.VeryProductive) / float64(this.TotalTimeSpent)) * 100,
+		(float64(this.Productive) / float64(this.TotalTimeSpent)) * 100,
+		(float64(this.Nuetral) / float64(this.TotalTimeSpent)) * 100,
+		(float64(this.Unproductive) / float64(this.TotalTimeSpent)) * 100,
+		(float64(this.VeryUnproductive) / float64(this.TotalTimeSpent)) * 100,
 	}
-	color.Cyan.Println("Time logged today:",this.TotalTimeSpent/3600," hours, ",this.TotalTimeSpent%60," minutes")
-	for i:=0;i<5;i++{
-		switch i{
+	color.Cyan.Println("Time logged today:", this.TotalTimeSpent/3600, " hours, ", this.TotalTimeSpent%60, " minutes")
+	for i := 0; i < 5; i++ {
+		switch i {
 		case 0:
 			color.Blue.Print("Very Productive    ")
 		case 1:
@@ -46,17 +46,16 @@ func (this* results) OutputHelper(){
 			color.Red.Print("Very Unproductive  ")
 		}
 		fmt.Print("[")
-		for j:=0.0;j<50;j++{
-			if j > percentages[i]/2{
+		for j := 0.0; j < 50; j++ {
+			if j > percentages[i]/2 {
 				fmt.Print("-")
-			}else{
+			} else {
 				fmt.Print("#")
 			}
 		}
 
 		fmt.Print("]")
-		fmt.Printf("%9.2f",percentages[i])
+		fmt.Printf("%9.2f", percentages[i])
 		fmt.Println("%")
 	}
 }
-
